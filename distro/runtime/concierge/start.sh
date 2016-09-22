69e73ce11b03518e560b2ba1d9bab8f74ebf22a7
Christoph Schauer <christoph@schauer-itconsulting.de>
2016-09-22 11:31:29 +0200

    Updated the start.sh script

--- distro/runtime/concierge/start.sh
+++ distro/runtime/concierge/start.sh
@@ -6,27 +6,33 @@ PROGNAME=`basename "$0"`
 RUNTIME_FOLDER=`cd "$DIRNAME/.."; pwd`
 BASE_FOLDER=`cd "$RUNTIME_FOLDER/.."; pwd`
 
-# JVM options
-ARGS="-Dsmarthome.logdir=$BASE_FOLDER/userdata/logs -Dsmarthome.servicepid=org.eclipse -Djetty.etc.config.urls=etc/jetty.xml,etc/jetty-deployer.xml,etc/jetty-selector.xml"
-
 # set ports for HTTP(S) server
-# TODO: check if already set
-HTTP_PORT=8080
-HTTPS_PORT=8443
+if [ "x$HTTP_PORT" = "x" ]; then
+    HTTP_PORT="8080"
+fi
 
+if [ "x$HTTPS_PORT" = "x" ]; then
+    HTTPS_PORT="8443"
+fi
+
+# Find the concierge framework jar
 MAIN=$(find framework -name "org.eclipse.concierge-5.0.0*.jar" | sort | tail -1);
 
-java $DEBUG_OPTS $ARGS \
+# Start in debog
+java $JAVA_OPTS \
 	-Dosgi.clean=true \
 	-Dorg.osgi.framework.storage=$BASE_FOLDER/userdata/storage \
-	-Declipse.ignoreApp=true \
 	-Dosgi.noShutdown=true \
+	-Declipse.ignoreApp=true \
+	-Dsmarthome.servicepid=org.eclipse \
+	-Dsmarthome.logdir=$BASE_FOLDER/userdata/logs \
 	-Dsmarthome.userdata=$BASE_FOLDER/userdata \
-	-Dlogback.configurationFile=$RUNTIME_FOLDER/etc/logback_debug.xml \
 	-Dsmarthome.servicecfg=$RUNTIME_FOLDER/etc/services.cfg \
+	-Dlogback.configurationFile=$RUNTIME_FOLDER/etc/logback_debug.xml \
 	-Dorg.quartz.properties=$RUNTIME_FOLDER/etc/quartz.properties \
 	-DmdnsName=smarthome \
 	-Djetty.home=$RUNTIME_FOLDER/ \
+	-Djetty.etc.config.urls=etc/jetty.xml,etc/jetty-deployer.xml,etc/jetty-selector.xml \
 	-Dorg.osgi.service.http.port=$HTTP_PORT \
 	-Dorg.osgi.service.http.port.secure=$HTTPS_PORT \
 	-Dfelix.fileinstall.dir=$BASE_FOLDER/addons \
